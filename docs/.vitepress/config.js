c9ec6a0a9f3f0bbc2ad5e151bfb49e78c314a2bc
wbchangpengyuan <wbchangpengyuan@lattebank.com>
2025-02-26 15:55:51 +0800

    feat: 添加文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,27 @@
 export default {
     title: 'Hello',
     description: 'description',
+    base: '/docs/',
+    cleanUrls: true,
     themeConfig: {
-        
-    }
+        siteTitle: '瓜娃子的秘密花园',
+        search: {
+            provider: 'local'
+        },      
+        nav: [
+            { text: '笔记', link: '/skill/index' }
+        ],
+        sidebar: {
+            '/skill/': [
+                { 
+                    text: 'CSS',
+                    items: [
+                        { text: 'zIndex', link: '/skill/css/zIndex/index' }
+                    ]
+                },
+                { text: 'JavaScript', link: '/skill/javascript/index' },
+            ]
+        }
+    },
+    srcDir:  "./"
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,9 @@
+---
+prev: false
+next: false
+layout: home
+---
+
 # Hello World
+
+222
